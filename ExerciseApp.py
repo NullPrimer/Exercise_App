cce4f9be574928989d441edc3c28960e58db7a0b
NullPrimer <80787285+NullPrimer@users.noreply.github.com>
2021-07-08 15:20:18 +0100

    Add files via upload
    Source Code.

---
+++ ExerciseApp.py
@@ -0,0 +1,22 @@
+# Array's
+import random
+Day_of_the_week =("Wednesday")
+print("This is your exercise for " + Day_of_the_week)
+if Day_of_the_week == "Monday":
+    print(random.choice(("Bench", "Flys", "Dumbell_Raise")))
+elif Day_of_the_week == "Tuesday":
+    print(random.choice(("Shoulder Press", "Lat_Raises", "Face_Pull")))
+elif Day_of_the_week == "Wednesday":
+    print(random.choice(("Seated_Row", "Lat_Pulldown", "Bent_OverRow")))
+elif Day_of_the_week == "Thursday":
+    print(random.choice(("Row", "Seated_Row's", "Bent over Rows", "Lat Pulldown")))
+elif Day_of_the_week == "Friday":
+    print(random.choice(("Shoulder Press","Lat Raises", "Cable Pulls")))
+elif Day_of_the_week == "Saturday":
+    print("Rest_Day")
+
+# Exellent. Review the user imput Line 3 in case you have to code for a users input. Good Work. 30/06/2021
+
+#def Monday():
+   # print("Bench")
+#Monday()
